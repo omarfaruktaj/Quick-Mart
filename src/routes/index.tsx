b7a0a352bf444f4783033c327252fa718fcda81d
omarfaruktaj <omarfaruk3393@gmail.com>
2024-05-26 09:56:15 +0600

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../app/root/layout";
+import Home from "../app/root/home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
