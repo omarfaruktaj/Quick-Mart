654f062da55f0bb62b5fca312e6afa47d8997b3a
omarfaruktaj <omarfaruk3393@gmail.com>
2024-05-26 10:40:49 +0600

    Navbar and footer added

--- src/app/root/layout.tsx
+++ src/app/root/layout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../../components/layout/navbar";
+import Footer from "../../components/layout/footer";
 
 export default function RootLayout() {
   return (
     <div>
-      RootLayout
+      <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
